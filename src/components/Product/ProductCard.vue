fa5a71b843e2babc2479f93c075656b8f399521f
Yoosuf <yoosufm@opusxenta.com>
2025-01-29 14:13:17 +0530

    Byond UI added

--- src/components/Product/ProductCard.vue
+++ src/components/Product/ProductCard.vue
@@ -1,16 +1,21 @@
 <template>
   <div class="p-4 border rounded">
-    <img v-if="product.images.length" :src="product.images[0]" class="w-full h-40 object-cover" />
+    <img
+      v-if="product.images.length"
+      :src="product.images[0]"
+      class="w-full h-40 object-cover"
+    />
     <h3 class="text-lg font-semibold">{{ product.title }}</h3>
     <p class="text-gray-700">${{ product.price }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
-import type { Product } from '@/types';
+import { defineProps } from "vue";
+import type { Product } from "@/types";
+import  Card  from "@ox/byond-design-system";
 
 const props = defineProps<{ product: Product }>();
 
-console.log('ProductCard', props.product.images[0]);
+console.log("ProductCard", props.product.images[0]);
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import ByondDesignSystem from "@ox/byond-design-system";
+import '@ox/byond-design-system/dist/style.css' // If styles are included
 
 const app = createApp(App);
 const pinia = createPinia();
 
+app.use(ByondDesignSystem);
 app.use(pinia);
 app.use(router); 
 
